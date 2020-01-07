214ef786af5bbaeb67fd8803b3e042dea71c99fc
Mohamed Gamal <Mohamed.abugalala@gmail.com>
2020-01-07 23:12:30 +0200

    cleaning up app

--- api/app.js
+++ api/app.js
@@ -10,28 +10,3 @@ require('./startup/middlewares')(app);
 require('./startup/routes')(app);
 
 module.exports = app;
-
-// ! example and will be removed
-// const { UserEntity } = require('./components/User/Entity');
-// setTimeout(async () => {
-//   try {
-//     const newUser = new UserEntity({
-//       fullName: 'Mohamed gamal',
-//       birthDateTs: new Date('9/4/1996').getTime(),
-//       email: 'mohmeds.aaa@gmail.cm',
-//       gender: 'male',
-//       government: 'Damietta',
-//       image: 'sample image',
-//       phone: '+25455588',
-//       job: { type: 'employee', description: 'work at 04' }
-//     });
-
-//     newUser.setPassword('12451245');
-
-//     await newUser.save();
-//     console.log('ddddddddddddddddddddddd', newUser.toJson());
-//   } catch (error) {
-//     console.log('eeeeeeeeeeeeee', error);
-//   }
-// }, 500);
-// const e = await UserEntity.loadEntityFromDbById('507f191e810c19729de860ea');
