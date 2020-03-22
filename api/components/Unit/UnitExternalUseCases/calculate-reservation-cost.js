8708d2d7763454b0fa479dfd21d70b557b8bdb59
Ahmed Mashhour <mashhou04@gmail.com>
2020-03-22 16:27:07 +0200

    setting get unit and get unit with owner ex

---
+++ api/components/Unit/UnitExternalUseCases/calculate-reservation-cost.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+//! only require Entity/model
+const model = require('../Models');
+
+// should have no implementation for any specific orm
+
+module.exports = ({ ApplicationError, logger, _ }) => async ({
+  hostelId,
+  fromts,
+  tots,
+  rooms
+}) => {
+  try {
+    const query = {
+      _id: hostelId,
+      isArchived: false
+    };
+    const select = 'rooms';
+    const checkExistence = await model.getOne({ query, select });
+    return 0;
+  } catch (error) {
+    return false;
+  }
+};
