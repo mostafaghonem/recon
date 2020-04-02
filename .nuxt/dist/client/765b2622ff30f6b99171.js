012e00bfdf106da1b03d43029adf09a2a25a11e3
Mohamed Farid <mohamedamal680@gmail.com>
2020-04-02 19:03:07 +0200

    build last front and fix hostels count

---
+++ .nuxt/dist/client/765b2622ff30f6b99171.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[35],{684:function(n,l,o){"use strict";o.r(l);var e=o(655).default,t=o(3),component=Object(t.a)(e,void 0,void 0,!1,null,null,null);l.default=component.exports}}]);
