4c0ca0a23eaf87e1cee7e60ac0fca5fa088457e4
Ahmed Mashhour <mashhou04@gmail.com>
2020-04-02 20:20:02 +0200

    Build UI for process browser fix

--- .nuxt/utils.js
+++ .nuxt/utils.js
@@ -133,7 +133,7 @@ export async function setContext (app, context) {
       payload: context.payload,
       error: context.error,
       base: '/',
-      env: {}
+      env: {"NODE_ENV":"local"}
     }
     // Only set once
     if (context.req) {
