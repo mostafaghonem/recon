eaff89b74f289867ab8b258b07e18c6c9377300b
Mohamed Farid <mohamedamal680@gmail.com>
2020-04-11 15:01:50 +0200

    build last front

--- .nuxt/dist/client/194599b0b3dd82759698.js
+++ .nuxt/dist/client/194599b0b3dd82759698.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD:.nuxt/dist/client/194599b0b3dd82759698.js
 (window.webpackJsonp=window.webpackJsonp||[]).push([[36],{692:function(n,l,o){"use strict";o.r(l);var e=o(662).default,t=o(2),component=Object(t.a)(e,void 0,void 0,!1,null,null,null);l.default=component.exports}}]);
-=======
-(window.webpackJsonp=window.webpackJsonp||[]).push([[34],{692:function(n,l,o){"use strict";o.r(l);var e=o(662).default,t=o(2),component=Object(t.a)(e,void 0,void 0,!1,null,null,null);l.default=component.exports}}]);
->>>>>>> 7a54cb5c05cad0218becfae3f77db20ad9dca658:.nuxt/dist/client/6d0b886ac265513f90c0.js
