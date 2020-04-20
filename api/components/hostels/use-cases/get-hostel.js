6afd30934953fa1f46f6e48c98a2bc6c86f9e83d
Ahmed Mashhour <mashhou04@gmail.com>
2020-04-20 20:42:42 +0200

    getting similar hotels with backup query

--- api/components/hostels/use-cases/get-hostel.js
+++ api/components/hostels/use-cases/get-hostel.js
@@ -8,10 +8,9 @@ module.exports = ({
   ApplicationError,
   logger,
   getReservedRoomCountByHotels,
-  getHostelsFavorability,
+  getSimilarHostels,
   accepted
 }) => async ({ hostelId, userId }) => {
-  let hostelsFavorability = {};
   const query = { _id: hostelId, status: accepted, isArchived: false };
   const select =
     'name description image currency address isHidden freeServices generalServices hostelServices entertainmentServices foodServices rooms rate totalUsersRated';
@@ -64,128 +63,6 @@ module.exports = ({
   }
   checkExistence.similarHostels = [];
 
-  const filter = {
-    _id: { $ne: checkExistence._id },
-    status: accepted,
-    $or: [
-      {
-        'address.government': String(
-          checkExistence.address.government
-        ).toLowerCase()
-      },
-      {
-        'address.street': {
-          $regex: new RegExp(checkExistence.address.street),
-          $options: 'i'
-        }
-      },
-      {
-        'address.highlight': String(
-          checkExistence.address.highlight
-        ).toLowerCase()
-      },
-      {
-        'address.nearTo': {
-          $regex: new RegExp(checkExistence.address.nearTo),
-          $options: 'i'
-        }
-      },
-      {
-        name: {
-          $regex: new RegExp(checkExistence.name),
-          $options: 'i'
-        }
-      },
-      { freeServices: checkExistence.freeServices },
-      {
-        generalServices: checkExistence.generalServices
-      },
-      {
-        hostelServices: checkExistence.hostelServices
-      },
-      {
-        entertainmentServices: checkExistence.entertainmentServices
-      },
-      {
-        foodServices: checkExistence.foodServices
-      }
-    ],
-    isHidden: false,
-    isArchived: false
-  };
-  const project = 'name image rate totalUsersRated rooms address.government';
-  const sort = { createdAt: 1 };
-  const hostels = await model.getMany({
-    query: filter,
-    select: project,
-    sort,
-    skip: 0,
-    limit: 4
-  });
-  if (userId) {
-    const hostelsIds = hostels && hostels.length ? hostels.map(o => o._id) : [];
-    hostelsFavorability = await getHostelsFavorability({
-      userId,
-      hostelsIds: [checkExistence._id, ...hostelsIds]
-    });
-    const favoriteExistance =
-      hostelsFavorability[checkExistence._id.toString()];
-    checkExistence.favorite = !!favoriteExistance;
-    checkExistence.displayFavorite = true;
-  }
-  if (hostels && hostels.length !== 0) {
-    const hostelsIds = [];
-    hostels.map(hostel => hostelsIds.push(hostel._id));
-    const newAvailableFrom =
-      new Date(availableFrom).getTime() || new Date().getTime();
-    const newAvailableTo =
-      availableTo || new Date().setDate(new Date().getDate() + 1);
-    const reservedHostels = await getReservedRoomCountByHotels(
-      hostelsIds,
-      newAvailableFrom,
-      new Date(newAvailableTo).getTime()
-    );
-    hostels.forEach(hostel => {
-      hostel.totalRooms = 0;
-      hostel.totalAvailableRooms = 0;
-      hostel.available = false;
-      if (hostel.rooms[0]) {
-        const getHostelData = reservedHostels.filter(
-          reservedHostel => String(reservedHostel._id) === String(hostel._id)
-        );
-        hostel.rooms.forEach(group => {
-          if (getHostelData[0]) {
-            const getGroupData = getHostelData[0].rooms.filter(
-              room => String(room.groupId) === String(group._id)
-            );
-            if (getGroupData[0]) {
-              hostel.totalRooms += Number(group.totalRooms);
-              hostel.totalAvailableRooms +=
-                Number(group.totalRooms) -
-                Number(getGroupData[0].totalReservedCount);
-              group.availableRooms =
-                Number(group.totalRooms) -
-                Number(getGroupData[0].totalReservedCount);
-              if (group.availableRooms > 0) {
-                group.available = true;
-                hostel.available = true;
-              }
-            }
-          } else {
-            hostel.totalRooms += Number(group.totalRooms);
-            hostel.totalAvailableRooms += Number(group.totalAvailableRooms);
-            group.availableRooms = Number(group.totalAvailableRooms);
-            if (group.availableRooms > 0) {
-              group.available = true;
-              hostel.available = true;
-            } else group.available = false;
-          }
-        });
-      }
-      hostel.favorite = !!hostelsFavorability[hostel._id.toString()];
-      hostel.displayFavorite = true;
-      checkExistence.similarHostels.push(hostel);
-    });
-  }
-  return checkExistence;
+  const response = await getSimilarHostels({ checkExistence, userId });
+  return response;
 };
