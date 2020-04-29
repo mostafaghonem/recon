eddf82a8acbf9f708cbcf413b7614f36f030cc76
Mohamed Farid <mohamedamal680@gmail.com>
2020-04-29 21:38:44 +0200

    build last front

--- .nuxt/middleware.js
+++ .nuxt/middleware.js
@@ -1,15 +1,15 @@
 const middleware = {}
 
-middleware['auth'] = require('../middleware/auth.js')
+middleware['auth'] = require('..\\middleware\\auth.js')
 middleware['auth'] = middleware['auth'].default || middleware['auth']
 
-middleware['isAdmin'] = require('../middleware/isAdmin.js')
+middleware['isAdmin'] = require('..\\middleware\\isAdmin.js')
 middleware['isAdmin'] = middleware['isAdmin'].default || middleware['isAdmin']
 
-middleware['isHouseOwner'] = require('../middleware/isHouseOwner.js')
+middleware['isHouseOwner'] = require('..\\middleware\\isHouseOwner.js')
 middleware['isHouseOwner'] = middleware['isHouseOwner'].default || middleware['isHouseOwner']
 
-middleware['isLoggedIn'] = require('../middleware/isLoggedIn.js')
+middleware['isLoggedIn'] = require('..\\middleware\\isLoggedIn.js')
 middleware['isLoggedIn'] = middleware['isLoggedIn'].default || middleware['isLoggedIn']
 
 export default middleware
