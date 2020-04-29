a2c2f7790d82c0be127578a385b2ace1e07e9d52
Ahmed Mashhour <mashhou04@gmail.com>
2020-04-29 02:49:23 +0200

    Build front for mashhour milestone-2 last edits

--- .nuxt/middleware.js
+++ .nuxt/middleware.js
@@ -9,4 +9,7 @@ middleware['isAdmin'] = middleware['isAdmin'].default || middleware['isAdmin']
 middleware['isHouseOwner'] = require('../middleware/isHouseOwner.js')
 middleware['isHouseOwner'] = middleware['isHouseOwner'].default || middleware['isHouseOwner']
 
+middleware['isLoggedIn'] = require('../middleware/isLoggedIn.js')
+middleware['isLoggedIn'] = middleware['isLoggedIn'].default || middleware['isLoggedIn']
+
 export default middleware
