bfae67edef8574c898a6950fe7fcbb915fdee464
Ahmed Mashhour <mashhou04@gmail.com>
2020-04-30 02:46:04 +0200

    order of token search

--- api/middlewares/authenticateMiddleware.js
+++ api/middlewares/authenticateMiddleware.js
@@ -5,17 +5,14 @@ const jwt = Promise.promisifyAll(jsonwebtoken);
 
 const isAuthenticated = async (req, res, next) => {
   const token =
+    req.cookies.sknToken ||
+    req.signedCookies.sknToken ||
     req.body['access-token'] ||
     req.query['access-token'] ||
-    req.headers['access-token'] ||
-    req.cookies.sknToken ||
-    req.signedCookies.sknToken;
-
-  console.log('authenticating the token', token);
+    req.headers['access-token'];
   if (token) {
     try {
       const decoded = await jwt.verify(token, process.env.jwtPrivateKey);
-      console.log('authenticating the user', decoded.id, decoded.fullName);
       req.user = decoded;
       req.user.token = token;
       req.decoded = decoded;
