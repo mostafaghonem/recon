6ea19a558be5c75d1965af75662339b3d997f51d
Mohamed Farid <mohamedamal680@gmail.com>
2020-06-17 14:19:04 +0200

    fix get office

--- api/components/User/user-external-use-cases/get-offices-favorability.js
+++ api/components/User/user-external-use-cases/get-offices-favorability.js
@@ -1,22 +1,18 @@
 const Model = require('../models/UserFavoriteOfficesIndex');
 
 // eslint-disable-next-line no-unused-vars
-module.exports = ({ ApplicationError }) => async ({ userId, OfficesIds }) => {
+module.exports = ({ ApplicationError }) => async ({ userId, officesIds }) => {
   if (!userId) throw new ApplicationError('You should pass userId');
-  console.log('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb');
-  console.log(userId);
-  console.log(OfficesIds);
-  console.log('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb');
-  if (!Array.isArray(OfficesIds))
+  if (!Array.isArray(officesIds))
     throw new ApplicationError(
       'You should pass ids as array to getOfficesFavorability (User external service)'
     );
   const result = {};
-  OfficesIds.forEach(id => {
+  officesIds.forEach(id => {
     result[id.toString()] = null;
   });
 
-  const Offices = await Model.getOfficesWithIds(userId, OfficesIds, {
+  const Offices = await Model.getOfficesWithIds(userId, officesIds, {
     favorite: true
   });
   Offices.map(u => {
