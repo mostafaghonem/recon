cdbff15dad85cd85671720aea7c48259b3e095f0
Ahmed Shibob <ahmed_shibob@outlook.com>
2020-06-19 23:40:56 +0200

    build milstone 4 and 5

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,46 +3,44 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _69914ea0 = () => interopDefault(import('..\\pages\\dashboard.vue' /* webpackChunkName: "pages_dashboard" */))
-const _59823587 = () => interopDefault(import('..\\pages\\enterNewPassword.vue' /* webpackChunkName: "pages_enterNewPassword" */))
-const _40464f4c = () => interopDefault(import('..\\pages\\forgetPassword.vue' /* webpackChunkName: "pages_forgetPassword" */))
-const _2c94a67c = () => interopDefault(import('..\\pages\\hotel\\index.vue' /* webpackChunkName: "pages_hotel_index" */))
-const _07a4cab0 = () => interopDefault(import('..\\pages\\hotelDetails\\index.vue' /* webpackChunkName: "pages_hotelDetails_index" */))
-const _3699a44e = () => interopDefault(import('..\\pages\\hotelGroups\\index.vue' /* webpackChunkName: "pages_hotelGroups_index" */))
-const _6e5065bf = () => interopDefault(import('..\\pages\\hotels\\index.vue' /* webpackChunkName: "pages_hotels_index" */))
-const _10e3baf5 = () => interopDefault(import('..\\pages\\login.vue' /* webpackChunkName: "pages_login" */))
-const _f80c87bc = () => interopDefault(import('..\\pages\\office\\index.vue' /* webpackChunkName: "pages_office_index" */))
-const _3ef2af50 = () => interopDefault(import('..\\pages\\officeDetails\\index.vue' /* webpackChunkName: "pages_officeDetails_index" */))
-const _61acabae = () => interopDefault(import('..\\pages\\officeGroups\\index.vue' /* webpackChunkName: "pages_officeGroups_index" */))
-const _1c8e425f = () => interopDefault(import('..\\pages\\offices\\index.vue' /* webpackChunkName: "pages_offices_index" */))
-const _ff109468 = () => interopDefault(import('..\\pages\\passwordCode.vue' /* webpackChunkName: "pages_passwordCode" */))
-const _620da835 = () => interopDefault(import('..\\pages\\profile.vue' /* webpackChunkName: "pages_profile" */))
-const _ef3d5246 = () => interopDefault(import('..\\pages\\registration.vue' /* webpackChunkName: "pages_registration" */))
-const _2e31ffea = () => interopDefault(import('..\\pages\\registrationCode.vue' /* webpackChunkName: "pages_registrationCode" */))
-const _b1b8ba02 = () => interopDefault(import('..\\pages\\registrationDone.vue' /* webpackChunkName: "pages_registrationDone" */))
-const _0109f766 = () => interopDefault(import('..\\pages\\rents.vue' /* webpackChunkName: "pages_rents" */))
-const _14942bf5 = () => interopDefault(import('..\\pages\\search-results.vue' /* webpackChunkName: "pages_search-results" */))
-const _7f43c427 = () => interopDefault(import('..\\pages\\units\\index.vue' /* webpackChunkName: "pages_units_index" */))
-const _b39f99de = () => interopDefault(import('..\\pages\\hotel\\add.vue' /* webpackChunkName: "pages_hotel_add" */))
-const _f3cf9bb8 = () => interopDefault(import('..\\pages\\hotel\\addGroup\\index.vue' /* webpackChunkName: "pages_hotel_addGroup_index" */))
-const _71cfb6f8 = () => interopDefault(import('..\\pages\\hotel\\edit\\index.vue' /* webpackChunkName: "pages_hotel_edit_index" */))
-const _71d4c7dd = () => interopDefault(import('..\\pages\\hotel\\editGroup\\index.vue' /* webpackChunkName: "pages_hotel_editGroup_index" */))
-const _06432b81 = () => interopDefault(import('..\\pages\\hotel\\vs-upload.vue' /* webpackChunkName: "pages_hotel_vs-upload" */))
-const _077b2b8b = () => interopDefault(import('..\\pages\\hotels\\search.vue' /* webpackChunkName: "pages_hotels_search" */))
-const _6ef09a71 = () => interopDefault(import('..\\pages\\office\\add.vue' /* webpackChunkName: "pages_office_add" */))
-const _57988984 = () => interopDefault(import('..\\pages\\office\\addGroup\\index.vue' /* webpackChunkName: "pages_office_addGroup_index" */))
-const _c63a8350 = () => interopDefault(import('..\\pages\\office\\edit\\index.vue' /* webpackChunkName: "pages_office_edit_index" */))
-const _5f414706 = () => interopDefault(import('..\\pages\\office\\editGroup\\index.vue' /* webpackChunkName: "pages_office_editGroup_index" */))
-const _00d0a4ae = () => interopDefault(import('..\\pages\\unit\\add.vue' /* webpackChunkName: "pages_unit_add" */))
-const _7fd7f076 = () => interopDefault(import('..\\pages\\unitDetails\\reservationForm.vue' /* webpackChunkName: "pages_unitDetails_reservationForm" */))
-const _14f39a23 = () => interopDefault(import('..\\pages\\units\\search.vue' /* webpackChunkName: "pages_units_search" */))
-const _16838ae6 = () => interopDefault(import('..\\pages\\hotel\\requests\\_id.vue' /* webpackChunkName: "pages_hotel_requests__id" */))
-const _2b338010 = () => interopDefault(import('..\\pages\\unit\\edit\\_id.vue' /* webpackChunkName: "pages_unit_edit__id" */))
-const _ffb6b104 = () => interopDefault(import('..\\pages\\unit\\requests\\_id.vue' /* webpackChunkName: "pages_unit_requests__id" */))
-const _09ba6791 = () => interopDefault(import('..\\pages\\checkouts\\_id.vue' /* webpackChunkName: "pages_checkouts__id" */))
-const _37d8b900 = () => interopDefault(import('..\\pages\\unitDetails\\_id.vue' /* webpackChunkName: "pages_unitDetails__id" */))
-const _3d1ab762 = () => interopDefault(import('..\\pages\\units\\_id.vue' /* webpackChunkName: "pages_units__id" */))
-const _473c17de = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages_index" */))
+const _272329bc = () => interopDefault(import('../pages/dashboard.vue' /* webpackChunkName: "pages/dashboard" */))
+const _95733a76 = () => interopDefault(import('../pages/enterNewPassword.vue' /* webpackChunkName: "pages/enterNewPassword" */))
+const _b5de97ec = () => interopDefault(import('../pages/forgetPassword.vue' /* webpackChunkName: "pages/forgetPassword" */))
+const _42852a45 = () => interopDefault(import('../pages/hotel/index.vue' /* webpackChunkName: "pages/hotel/index" */))
+const _1bc96853 = () => interopDefault(import('../pages/hotelDetails/index.vue' /* webpackChunkName: "pages/hotelDetails/index" */))
+const _197787b9 = () => interopDefault(import('../pages/hotelGroups/index.vue' /* webpackChunkName: "pages/hotelGroups/index" */))
+const _1a5155e4 = () => interopDefault(import('../pages/hotels/index.vue' /* webpackChunkName: "pages/hotels/index" */))
+const _2f912f12 = () => interopDefault(import('../pages/login.vue' /* webpackChunkName: "pages/login" */))
+const _0a5de310 = () => interopDefault(import('../pages/notification/index.vue' /* webpackChunkName: "pages/notification/index" */))
+const _84890812 = () => interopDefault(import('../pages/officeDetails/index.vue' /* webpackChunkName: "pages/officeDetails/index" */))
+const _363b20d6 = () => interopDefault(import('../pages/officeGroups/index.vue' /* webpackChunkName: "pages/officeGroups/index" */))
+const _51b3f488 = () => interopDefault(import('../pages/offices/index.vue' /* webpackChunkName: "pages/offices/index" */))
+const _4170320a = () => interopDefault(import('../pages/passwordCode.vue' /* webpackChunkName: "pages/passwordCode" */))
+const _99a80592 = () => interopDefault(import('../pages/profile.vue' /* webpackChunkName: "pages/profile" */))
+const _4959d31b = () => interopDefault(import('../pages/registration.vue' /* webpackChunkName: "pages/registration" */))
+const _ec13a5b0 = () => interopDefault(import('../pages/registrationCode.vue' /* webpackChunkName: "pages/registrationCode" */))
+const _02e7d03d = () => interopDefault(import('../pages/registrationDone.vue' /* webpackChunkName: "pages/registrationDone" */))
+const _4f44b630 = () => interopDefault(import('../pages/rents.vue' /* webpackChunkName: "pages/rents" */))
+const _795e90b3 = () => interopDefault(import('../pages/search-results.vue' /* webpackChunkName: "pages/search-results" */))
+const _36d41240 = () => interopDefault(import('../pages/units/index.vue' /* webpackChunkName: "pages/units/index" */))
+const _f6919558 = () => interopDefault(import('../pages/hotel/add.vue' /* webpackChunkName: "pages/hotel/add" */))
+const _70c5727e = () => interopDefault(import('../pages/hotel/addGroup/index.vue' /* webpackChunkName: "pages/hotel/addGroup/index" */))
+const _785f79aa = () => interopDefault(import('../pages/hotel/edit/index.vue' /* webpackChunkName: "pages/hotel/edit/index" */))
+const _f9a11cea = () => interopDefault(import('../pages/hotel/editGroup/index.vue' /* webpackChunkName: "pages/hotel/editGroup/index" */))
+const _48422f84 = () => interopDefault(import('../pages/hotel/vs-upload.vue' /* webpackChunkName: "pages/hotel/vs-upload" */))
+const _48cf7ff4 = () => interopDefault(import('../pages/hotels/search.vue' /* webpackChunkName: "pages/hotels/search" */))
+const _e2f48da0 = () => interopDefault(import('../pages/office/add.vue' /* webpackChunkName: "pages/office/add" */))
+const _5e812d22 = () => interopDefault(import('../pages/office/addGroup/index.vue' /* webpackChunkName: "pages/office/addGroup/index" */))
+const _19793678 = () => interopDefault(import('../pages/unit/add.vue' /* webpackChunkName: "pages/unit/add" */))
+const _6b57144d = () => interopDefault(import('../pages/unitDetails/reservationForm.vue' /* webpackChunkName: "pages/unitDetails/reservationForm" */))
+const _4f6d0f2a = () => interopDefault(import('../pages/units/search.vue' /* webpackChunkName: "pages/units/search" */))
+const _58566868 = () => interopDefault(import('../pages/hotel/requests/_id.vue' /* webpackChunkName: "pages/hotel/requests/_id" */))
+const _068e1824 = () => interopDefault(import('../pages/unit/edit/_id.vue' /* webpackChunkName: "pages/unit/edit/_id" */))
+const _1d17d328 = () => interopDefault(import('../pages/unit/requests/_id.vue' /* webpackChunkName: "pages/unit/requests/_id" */))
+const _bc253fb4 = () => interopDefault(import('../pages/checkouts/_id.vue' /* webpackChunkName: "pages/checkouts/_id" */))
+const _19fedc52 = () => interopDefault(import('../pages/unitDetails/_id.vue' /* webpackChunkName: "pages/unitDetails/_id" */))
+const _30c1a4a8 = () => interopDefault(import('../pages/units/_id.vue' /* webpackChunkName: "pages/units/_id" */))
+const _1e8fc560 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -62,163 +60,155 @@ export const routerOptions = {
 
   routes: [{
     path: "/dashboard",
-    component: _69914ea0,
+    component: _272329bc,
     name: "dashboard"
   }, {
     path: "/enterNewPassword",
-    component: _59823587,
+    component: _95733a76,
     name: "enterNewPassword"
   }, {
     path: "/forgetPassword",
-    component: _40464f4c,
+    component: _b5de97ec,
     name: "forgetPassword"
   }, {
     path: "/hotel",
-    component: _2c94a67c,
+    component: _42852a45,
     name: "hotel"
   }, {
     path: "/hotelDetails",
-    component: _07a4cab0,
+    component: _1bc96853,
     name: "hotelDetails"
   }, {
     path: "/hotelGroups",
-    component: _3699a44e,
+    component: _197787b9,
     name: "hotelGroups"
   }, {
     path: "/hotels",
-    component: _6e5065bf,
+    component: _1a5155e4,
     name: "hotels"
   }, {
     path: "/login",
-    component: _10e3baf5,
+    component: _2f912f12,
     name: "login"
   }, {
-    path: "/office",
-    component: _f80c87bc,
-    name: "office"
+    path: "/notification",
+    component: _0a5de310,
+    name: "notification"
   }, {
     path: "/officeDetails",
-    component: _3ef2af50,
+    component: _84890812,
     name: "officeDetails"
   }, {
     path: "/officeGroups",
-    component: _61acabae,
+    component: _363b20d6,
     name: "officeGroups"
   }, {
     path: "/offices",
-    component: _1c8e425f,
+    component: _51b3f488,
     name: "offices"
   }, {
     path: "/passwordCode",
-    component: _ff109468,
+    component: _4170320a,
     name: "passwordCode"
   }, {
     path: "/profile",
-    component: _620da835,
+    component: _99a80592,
     name: "profile"
   }, {
     path: "/registration",
-    component: _ef3d5246,
+    component: _4959d31b,
     name: "registration"
   }, {
     path: "/registrationCode",
-    component: _2e31ffea,
+    component: _ec13a5b0,
     name: "registrationCode"
   }, {
     path: "/registrationDone",
-    component: _b1b8ba02,
+    component: _02e7d03d,
     name: "registrationDone"
   }, {
     path: "/rents",
-    component: _0109f766,
+    component: _4f44b630,
     name: "rents"
   }, {
     path: "/search-results",
-    component: _14942bf5,
+    component: _795e90b3,
     name: "search-results"
   }, {
     path: "/units",
-    component: _7f43c427,
+    component: _36d41240,
     name: "units"
   }, {
     path: "/hotel/add",
-    component: _b39f99de,
+    component: _f6919558,
     name: "hotel-add"
   }, {
     path: "/hotel/addGroup",
-    component: _f3cf9bb8,
+    component: _70c5727e,
     name: "hotel-addGroup"
   }, {
     path: "/hotel/edit",
-    component: _71cfb6f8,
+    component: _785f79aa,
     name: "hotel-edit"
   }, {
     path: "/hotel/editGroup",
-    component: _71d4c7dd,
+    component: _f9a11cea,
     name: "hotel-editGroup"
   }, {
     path: "/hotel/vs-upload",
-    component: _06432b81,
+    component: _48422f84,
     name: "hotel-vs-upload"
   }, {
     path: "/hotels/search",
-    component: _077b2b8b,
+    component: _48cf7ff4,
     name: "hotels-search"
   }, {
     path: "/office/add",
-    component: _6ef09a71,
+    component: _e2f48da0,
     name: "office-add"
   }, {
     path: "/office/addGroup",
-    component: _57988984,
+    component: _5e812d22,
     name: "office-addGroup"
   }, {
-    path: "/office/edit",
-    component: _c63a8350,
-    name: "office-edit"
-  }, {
-    path: "/office/editGroup",
-    component: _5f414706,
-    name: "office-editGroup"
-  }, {
     path: "/unit/add",
-    component: _00d0a4ae,
+    component: _19793678,
     name: "unit-add"
   }, {
     path: "/unitDetails/reservationForm",
-    component: _7fd7f076,
+    component: _6b57144d,
     name: "unitDetails-reservationForm"
   }, {
     path: "/units/search",
-    component: _14f39a23,
+    component: _4f6d0f2a,
     name: "units-search"
   }, {
     path: "/hotel/requests/:id?",
-    component: _16838ae6,
+    component: _58566868,
     name: "hotel-requests-id"
   }, {
     path: "/unit/edit/:id?",
-    component: _2b338010,
+    component: _068e1824,
     name: "unit-edit-id"
   }, {
     path: "/unit/requests/:id?",
-    component: _ffb6b104,
+    component: _1d17d328,
     name: "unit-requests-id"
   }, {
     path: "/checkouts/:id?",
-    component: _09ba6791,
+    component: _bc253fb4,
     name: "checkouts-id"
   }, {
     path: "/unitDetails/:id?",
-    component: _37d8b900,
+    component: _19fedc52,
     name: "unitDetails-id"
   }, {
     path: "/units/:id",
-    component: _3d1ab762,
+    component: _30c1a4a8,
     name: "units-id"
   }, {
     path: "/",
-    component: _473c17de,
+    component: _1e8fc560,
     name: "index"
   }],
 
