15580fa10a61de9ca84d5dabc4e3916854df9f39
Mohamed Farid <mohamedamal680@gmail.com>
2020-06-24 07:08:45 +0200

    try to see user permissions

--- api/components/Unit/Controller/add-unit.js
+++ api/components/Unit/Controller/add-unit.js
@@ -1,16 +1,18 @@
 const { addUnit } = require('../UseCase');
 
 module.exports = () => {
-    return async (req, res, next) => {
-        try {
-            const addedUnit = await addUnit({ ...req.body, userId: req.user.id });
-
-            return res.status(200).json({
-                message: 'Unit has been added successfully!',
-                unitId: addedUnit
-            });
-        } catch (e) {
-            return next(e);
-        }
-    };
+  return async (req, res, next) => {
+    try {
+      const addedUnit = await addUnit({ ...req.body, userId: req.user.id });
+      console.log('aaaaaaaaaaaaaaaaaaaaaaa');
+      console.log(req.user);
+      console.log('aaaaaaaaaaaaaaaaaaaaaaa');
+      return res.status(200).json({
+        message: 'Unit has been added successfully!',
+        unitId: addedUnit
+      });
+    } catch (e) {
+      return next(e);
+    }
+  };
 };
