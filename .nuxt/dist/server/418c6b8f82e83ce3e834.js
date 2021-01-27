3faa4f97843b9e74af193309ffdcbb9f0e56a8e0
Ahmed Mashhour <mpghknown@gmail.com>
2021-01-27 08:07:16 +0200

    second production build

---
+++ .nuxt/dist/server/418c6b8f82e83ce3e834.js
@@ -0,0 +1,2 @@
+exports.ids=[33],exports.modules={377:function(e,t,r){"use strict";r.r(t);var d=r(14),o={middleware({route:e,redirect:t}){const path=e.path,base=d.b.defaults.baseURL.replace("/api/","");return t(encodeURI(base+path))}},l=r(10),component=Object(l.a)(o,void 0,void 0,!1,null,null,"ad7cad4e");t.default=component.exports}};
+//# sourceMappingURL=418c6b8f82e83ce3e834.js.map
