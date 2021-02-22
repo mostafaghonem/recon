4611602f39e2f694b4246166c2042aca1e642703
Ahmed Mashhour <mashhou04@gmail.com>
2021-02-22 04:36:23 +0200

    brain storming influences

--- api/startup/db.js
+++ api/startup/db.js
@@ -8,9 +8,11 @@ module.exports = {
   connect: () => {
     // const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
     const { DB_HOST, DB_PORT, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env;
+    let db = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
 
-    const db = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
-
+    if (!DB_USERNAME || !DB_PASSWORD) {
+      db = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+    }
     mongoose.connect(db, { useNewUrlParser: true, poolSize: 10 }, err => {
       if (err) {
         throw err;
