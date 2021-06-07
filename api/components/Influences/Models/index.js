2495333365fe39e2f8c0c8f892c33d6773b9f521
Ahmed Mashhour <mashhou04@gmail.com>
2021-06-07 23:55:47 +0200

    finalizing release date update

--- api/components/Influences/Models/index.js
+++ api/components/Influences/Models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const ObjectId = mongoose.Types.ObjectId;
-const makeInfluenceModel = require('./InfluenceModel');
+const makeInfluenceModel = require('./influenceModel');
 const GenericModel = require('../../shared/models/GenericModel');
 
 module.exports = makeInfluenceModel({
--- api/components/Soldier/UseCase/edit-soldier.js
+++ api/components/Soldier/UseCase/edit-soldier.js
@@ -5,6 +5,7 @@ const model = require('../Models');
 // should have no implementation for any specific orm
 
 module.exports = ({ ApplicationError, logger }) => async ({
+  releaseDateOnly,
   userId,
   soldierId,
   tripleNumber,
@@ -32,48 +33,59 @@ module.exports = ({ ApplicationError, logger }) => async ({
   isArchived
 }) => {
   let unit;
+  let update;
+  
   const filter = { _id: soldierId, isArchived: false };
   const checkExistence = await model.exists({ filter });
   if (!checkExistence)
     throw new ApplicationError('.نأسف ، لا يمكننا العثور على هذا المجند', 403);
-  if (unitId) {
-    unit = {
+
+  if (releaseDateOnly) {
+    update = {
+      releaseDate
+    };
+  } else {
+    if (unitId) {
+      unit = {
+        unitId,
+        divisionId: divisionId || undefined
+      };
+    }
+    update = {
+      recruitmentArea,
+      address,
+      militaryId,
+      recordId,
+      nationalId,
+      name,
+      fullName,
+      force,
+      army,
+      joinDate,
+      releaseDate,
+      birthDate,
+      recruitmentLevel,
+      educationRank,
+      influences,
       unitId,
-      divisionId: divisionId || undefined
+      divisionId,
+      treatment,
+      units,
+      situation,
+      unit,
+      isHidden,
+      isArchived
     };
   }
-  const update = {
-    tripleNumber: {
+
+  if (tripleNumber) {
+    update.tripleNumber = {
       year: tripleNumber.first,
       value: Object.keys(tripleNumber)
         .map(o => tripleNumber[o])
         .join('/')
-    },
-    recruitmentArea,
-    address,
-    militaryId,
-    recordId,
-    nationalId,
-    name,
-    fullName,
-    force,
-    army,
-    joinDate,
-    releaseDate,
-    birthDate,
-    recruitmentLevel,
-    educationRank,
-    influences,
-    unitId,
-    divisionId,
-    treatment,
-    units,
-    situation,
-    unit,
-    isHidden,
-    isArchived
-  };
-
+    };
+  }
   Object.keys(update).map(key => {
     if (update[key] === null) {
       update[key] = undefined;
